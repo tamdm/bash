458dc3f60c7de75fba134d4a3208c09cae001cf4
TamDM <48487569+tamdm@users.noreply.github.com>
2022-04-05 08:35:05 +0700

    Update send_alert.sh

--- send_alert.sh
+++ send_alert.sh
@@ -171,7 +171,7 @@ function sendMessageToTelegram()
 {
     local IP=$(curl -s https://ip.vinahost.vn || (hostname -I | awk '{print $1}'))
     local token_id="1126087523:AAG38a7Fm_ZJDey1LXFgdJZLH_WLYpUeWtk"
-    local group_id="-649404770"
+    local group_id="-604759080"
     # local group_id="-1291900277"
     local -r URL="https://api.telegram.org/bot$token_id/sendMessage"
 
