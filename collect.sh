6580150dff241401cd7f9bc41b28046574196dc8
TamDM <48487569+tamdm@users.noreply.github.com>
2023-01-21 00:32:16 +0700

    Create collect.sh

---
+++ collect.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+CPU=$(uptime | awk -F ":" {'print $5'} | awk '{print $2}')
+vCORE=$(lscpu | head -n 5 | grep -w "CPU(s):" | awk '{print $2}')
+RAM=$(free -h | grep -vE "total|Swap" | awk {'print $3"/"$2'})
+DISK=$(df -hF zfs | grep -w "/" |  awk {'print $3"/"$2" ~ "$5'})
+
+echo "CPU: ${CPU}"
+echo "vCORE: ${vCORE}"
+echo "RAM: ${RAM}"
+echo "DISK: ${DISK}"
