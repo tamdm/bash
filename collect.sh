fa684dbd4c72e0c343c33d976e9a0d5857ff5fb8
TamDM <48487569+tamdm@users.noreply.github.com>
2023-01-27 15:49:41 +0700

    Update collect.sh

--- collect.sh
+++ collect.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-CPU=$(uptime | awk -F ":" {'print $5'} | awk '{print $2}'| rev | cut -c2- | rev)
+CPU=$(cat /proc/loadavg | awk '{print $1}')
 vCORE=$(/usr/bin/lscpu | grep "CPU(s):" | grep -v "NUMA" | awk '{print $2}')
 RAM=$(free -h | grep -vE "total|Swap" | awk {'print $3"/"$2'})
-DISK=$(df -hF zfs | grep -w "/" |  awk {'print $3"/"$2" ~ "$5'})
+#DISK=$(df -hF zfs | grep -w "/" |  awk {'print $3"/"$2" ~ "$5'})
+DISK=$(zfs list | grep -w "/" | awk '{print $3"/"$2}')
 
 echo "CPU: ${CPU}"
 echo "vCORE: ${vCORE}"
