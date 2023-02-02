e7c22071d0e1b94719c0583648873e658810189e
TamDM <48487569+tamdm@users.noreply.github.com>
2023-02-02 10:26:29 +0700

    Update collect.sh

--- collect.sh
+++ collect.sh
@@ -4,7 +4,7 @@ CPU=$(cat /proc/loadavg | awk '{print $1}')
 vCORE=$(/usr/bin/lscpu | grep "CPU(s):" | grep -v "NUMA" | awk '{print $2}')
 RAM=$(free -h | grep -vE "total|Swap" | awk {'print $3"/"$2'})
 DISK=$(df -hF zfs | grep -w "/" |  awk {'print $3"/"$2" ~ "$5'})
-#DISK=$(zfs list | grep -w "/" | awk '{print $3"/"$2}')
+#DISK=$(zfs list | grep -w "/" | awk '{print $2"/"$3}')
 
 echo "CPU: ${CPU}"
 echo "vCORE: ${vCORE}"
