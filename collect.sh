4724183059931c543fcecd3acd3912502bcfffea
TamDM <48487569+tamdm@users.noreply.github.com>
2023-03-23 16:46:58 +0700

    Update collect.sh

--- collect.sh
+++ collect.sh
@@ -2,8 +2,9 @@
 
 
 CPU=$( awk '{print $1}' < /proc/loadavg )
-vCORE=$( /usr/bin/lscpu | grep "CPU(s):" | grep -v "NUMA" | awk '{ print $2 }' )
-RAM=$( free -h | grep -vE "total|Swap" | awk '{ print $3 "/" $2 }' )
+vCORE=$( /usr/bin/lscpu | grep -w "CPU(s):" | awk 'NR==1 { print $2 }' )
+#RAM=$( free -h | grep -vE "total|Swap" | awk '{ print $3 "/" $2 }' )
+RAM=$( free -h | awk '/^Mem/ { print $3 " / " $2 }' )
 DISK=$( df -hF zfs | grep -w "/" |  awk '{ print $3 "/" $2 " ~ " $5 }' )
 #DISK=$(zfs list | grep -w "/" | awk '{print $3"/"$2}')
 
