11f1fdd4b0acfed9c75bd269ff55f43b78120b99
TamDM <48487569+tamdm@users.noreply.github.com>
2023-12-20 15:18:42 +0700

    Update collect.sh

--- collect.sh
+++ collect.sh
@@ -1,17 +1,5 @@
 #!/usr/bin/env bash
 
-# CPU=$( awk '{print $1}' < /proc/loadavg )
-# vCORE=$( /usr/bin/lscpu | grep -w "CPU(s):" | awk 'NR==1 { print $2 }' )
-# #RAM=$( free -h | grep -vE "total|Swap" | awk '{ print $3 "/" $2 }' )
-# RAM=$( free -h | awk '/^Mem/ { print $3 " / " $2 }' )
-# DISK=$( df -hF zfs | grep -w "/" |  awk '{ print $3 "/" $2 " ~ " $5 }' )
-# #DISK=$(zfs list | grep -w "/" | awk '{print $3"/"$2}')
-
-# echo "CPU: ${CPU}"
-# echo "vCORE: ${vCORE}"
-# echo "RAM: ${RAM}"
-# echo "DISK: ${DISK}"
-
 # ---------------------------------------------------------------------------
 # install.sh - Bash shell script
 # Copyright 2012-2020, Tam Do Minh <dominhtam.94@gmail.com> - Skype <dominhtam.94>
